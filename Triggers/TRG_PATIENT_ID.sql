4c53f03d410b8f8edd9ebdf0698eb8b1526f66a9
Vasavi <vasavivishwakarma235@gmail.com>
2026-06-28 11:26:54 +0530

    Update TRG_PATIENT_ID.sql

--- Triggers/TRG_PATIENT_ID.sql
+++ Triggers/TRG_PATIENT_ID.sql
@@ -1,1 +1,17 @@
+/*
+-------------------------------------------------------
+Hospital Management System
+Trigger : TRG_PATIENT_ID
+Author  : Vasavi
+-------------------------------------------------------
+*/
 
+CREATE OR REPLACE TRIGGER TRG_PATIENT_ID
+BEFORE INSERT ON PATIENTS
+FOR EACH ROW
+BEGIN
+    :NEW.PATIENT_ID := PATIENT_SEQ.NEXTVAL;
+END;
+/
+
+ALTER TRIGGER TRG_PATIENT_ID ENABLE;
