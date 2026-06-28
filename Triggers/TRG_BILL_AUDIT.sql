2d07d54c1afe11d4ea96551bd517a1d30a2303c8
Vasavi <vasavivishwakarma235@gmail.com>
2026-06-28 11:21:58 +0530

    Update TRG_BILL_AUDIT.sql

--- Triggers/TRG_BILL_AUDIT.sql
+++ Triggers/TRG_BILL_AUDIT.sql
@@ -1,1 +1,36 @@
+/*
+-------------------------------------------------------
+Hospital Management System
+Trigger : TRG_BILL_AUDIT
+Author  : Vasavi
+-------------------------------------------------------
+*/
 
+CREATE OR REPLACE TRIGGER TRG_BILL_AUDIT
+AFTER UPDATE ON BILLS
+FOR EACH ROW
+DECLARE
+    V_USER VARCHAR2(50);
+BEGIN
+    V_USER := USER;
+
+    INSERT INTO AUDIT_LOG
+    (
+        LOG_ID,
+        TABLE_NAME,
+        ACTION_TYPE,
+        ACTION_DATE,
+        USER_NAME
+    )
+    VALUES
+    (
+        AUDIT_SEQ.NEXTVAL,
+        'BILLS',
+        'UPDATE',
+        SYSDATE,
+        V_USER
+    );
+END;
+/
+
+ALTER TRIGGER TRG_BILL_AUDIT ENABLE;
