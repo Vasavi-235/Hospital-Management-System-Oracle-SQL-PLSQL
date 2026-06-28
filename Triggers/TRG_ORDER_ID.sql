134bc9c207b5a5a41a848fb32c3bae17873b72ca
Vasavi <vasavivishwakarma235@gmail.com>
2026-06-28 11:26:30 +0530

    Update TRG_ORDER_ID.sql

--- Triggers/TRG_ORDER_ID.sql
+++ Triggers/TRG_ORDER_ID.sql
@@ -1,1 +1,19 @@
+/*
+-------------------------------------------------------
+Hospital Management System
+Trigger : TRG_ORDER_ID
+Author  : Vasavi
+-------------------------------------------------------
+*/
 
+CREATE OR REPLACE TRIGGER TRG_ORDER_ID
+BEFORE INSERT ON LAB_ORDERS
+FOR EACH ROW
+BEGIN
+    IF :NEW.ORDER_ID IS NULL THEN
+        :NEW.ORDER_ID := ORDER_SEQ.NEXTVAL;
+    END IF;
+END;
+/
+
+ALTER TRIGGER TRG_ORDER_ID ENABLE;
